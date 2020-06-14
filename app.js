35b87715a4e398553e0bf9938583084418540cfd
Diego <drodriguez18@ucol.mx>
2020-06-13 23:08:00 -0500

    app Terminada Clima.

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
-//variavles
+//variables
 const api =
 {
     key:'950568ddd39eb5c425cc70889b304a39',
     url: 'http://api.openweathermap.org/data/2.5/'
 } 
 const searchBox = document.querySelector('.search-box');
-
 
 //evenlisteners
 eventos();
@@ -15,23 +14,52 @@ function eventos(){
     searchBox.addEventListener('blur', cargarInfo);
 };
 
-
-//FINCIONES
+//FUNCIONES
 function cargarInfo(e){
     let ciudad =e.target.value
     cargarDatos(ciudad);
+    
 }
-async function cargarDatos(ciudad){
-    await fetch(`${api.url}find?q=${ciudad}&units=metric&appid=${api.key}`)
+ function cargarDatos(ciudad){
+     fetch(`${api.url}find?q=${ciudad}&units=metric&appid=${api.key}`)
         .then(respuesta => {
             return respuesta.json();
         })
         .then(data => {
-           mostrarResultados(data);
+            mostrarResultados(data);
         })
 }
- function mostrarResultados(data){
-    console.log(data);
+ function mostrarResultados(info){
+
+    let lugar = info.list[0];
+
+    let uiCiudad = document.querySelector('.contry');
+    uiCiudad.innerHTML = `${lugar.name},${lugar.sys.country}`;
+
+    let fechaActual = new Date();
+    let uiFecha = document.querySelector('.date');
+    uiFecha.innerHTML = construirFecha(fechaActual);
+
+    let uiGrados = document.querySelector('.tiempo');
+    uiGrados.innerHTML = `${lugar.main.temp}c<span>°</span>`;
+
+    let uiClima = document.querySelector('.clima');
+    uiClima.innerHTML = `${lugar.weather[0].main}`;
+}
+ function construirFecha(fechaActual){
+    let meses = ["Januar","Februar", "März","April",
+                "Mai","Juni","Juli","August",
+                "September","Oktober","November","dezember"];
+
+    let dias = ["Montag","Dienstag","Mittwoch","Donnerstag",
+                "Freitag","Samstag","Sonntag"];
+
+    let año = fechaActual.getFullYear();
+    let mes = meses[fechaActual.getMonth()];
+    let dia = dias[fechaActual.getDay()];
+    let diaNumero = fechaActual.getDate();
+
+    return `${dia} ${diaNumero} ${mes} ${año}` ;
 }
 
 
@@ -42,6 +70,9 @@ async function cargarDatos(ciudad){
 
 
 
+
+
+//https://www.youtube.com/watch?v=n4dtwWgRueI
 
 
 
