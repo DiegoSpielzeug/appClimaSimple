11f7869f0a6e3ee971e2446c8822147876774c5c
Diego <drodriguez18@ucol.mx>
2020-06-13 23:27:31 -0500

    se agregan comentarios

--- app.js
+++ app.js
@@ -15,42 +15,54 @@ function eventos(){
 };
 
 //FUNCIONES
+//Accedemos a la informacion que esta escrita en el input
 function cargarInfo(e){
     let ciudad =e.target.value
+    //ejecutamos la funcion
     cargarDatos(ciudad);
-    
 }
+//Accedemos a la API y extraemos los datos en JSON
  function cargarDatos(ciudad){
      fetch(`${api.url}find?q=${ciudad}&units=metric&appid=${api.key}`)
         .then(respuesta => {
             return respuesta.json();
         })
         .then(data => {
-            mostrarResultados(data);
+           return mostrarResultados(data);
         })
 }
+//Esta funcion muestra los datos obtenidos de la API en el DOM
  function mostrarResultados(info){
 
+    //creamos una variable para acceder a los vatos con mayor facilidad.
     let lugar = info.list[0];
 
+    //seleccionamos el elemento donde sera remplazada la informacion que extragimos del API Y la cambiamos.
     let uiCiudad = document.querySelector('.contry');
     uiCiudad.innerHTML = `${lugar.name},${lugar.sys.country}`;
 
+    
+    /*creamos una variable para obtener la fecha actual e incertamos una funcion que construira la fecha
+    //seleccionamos el elemento donde sera remplazada la informacion que extragimos del API Y la cambiamos.*/
     let fechaActual = new Date();
     let uiFecha = document.querySelector('.date');
     uiFecha.innerHTML = construirFecha(fechaActual);
 
+    //seleccionamos el elemento donde sera remplazada la informacion que extragimos del API Y la cambiamos.
     let uiGrados = document.querySelector('.tiempo');
     uiGrados.innerHTML = `${lugar.main.temp}c<span>°</span>`;
 
+    //seleccionamos el elemento donde sera remplazada la informacion que extragimos del API Y la cambiamos.
     let uiClima = document.querySelector('.clima');
     uiClima.innerHTML = `${lugar.weather[0].main}`;
 }
+//es la funcion que construye la fecha
  function construirFecha(fechaActual){
+    //el metodo con el que otenemos los meses retorna el numero del mes entonces creamos un array para elegir el mes segun el numero que arroje
     let meses = ["Januar","Februar", "März","April",
                 "Mai","Juni","Juli","August",
                 "September","Oktober","November","dezember"];
-
+    // igual que la variable meses.
     let dias = ["Montag","Dienstag","Mittwoch","Donnerstag",
                 "Freitag","Samstag","Sonntag"];
 
@@ -59,6 +71,7 @@ function cargarInfo(e){
     let dia = dias[fechaActual.getDay()];
     let diaNumero = fechaActual.getDate();
 
+    //retornamos lo anterior de una manera acomodada para poder ser agregado al DOM
     return `${dia} ${diaNumero} ${mes} ${año}` ;
 }
 
@@ -72,18 +85,7 @@ function cargarInfo(e){
 
 
 
-//https://www.youtube.com/watch?v=n4dtwWgRueI
 
 
 
 
-
-
-
-
-
-
-
-
-
-///colima&units=metric&appid=
